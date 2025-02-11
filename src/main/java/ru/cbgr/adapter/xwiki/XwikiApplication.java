4b67a541796631f8d6e070ac500241fe23caa3a3
Чумаченко Артем Александрович (4104080) <chumachenko@dev.vtb.ru>
2025-02-11 12:03:29 +0300

    init

---
+++ src/main/java/ru/cbgr/adapter/xwiki/XwikiApplication.java
@@ -0,0 +1,13 @@
+package ru.cbgr.adapter.xwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XwikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/cbgr/adapter/xwiki/XwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cbgr.adapter.xwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
